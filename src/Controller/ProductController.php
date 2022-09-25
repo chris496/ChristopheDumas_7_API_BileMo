c33982adde58ca81f0d7173e5904dd4069e07d71
Christophe Dumas <cdumas@servitex.fr>
2022-09-25 20:12:32 +0200

    📦 NEW: install serializer + route api/products

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+    #[Route('api/products', name: 'product', methods: ['GET'])]
+    public function getProductList(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $productList = $productRepository->findAll();
+        $jsonProductList = $serializer->serialize($productList, 'json');
+        
+        return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
+    }
+}
