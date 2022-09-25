315e9ad500ca1ac5c6727504f2c9046d945ccc40
Christophe Dumas <cdumas@servitex.fr>
2022-09-25 23:00:33 +0200

    📦 NEW: add a new user (temp)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
@@ -28,9 +29,9 @@ class UserController extends AbstractController
     public function getOneUser(int $idClient, int $id, User $user, UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
     {
         $oneUser = $userRepository->findUser($idClient, $id);
-        $jsonUserList = $serializer->serialize($oneUser, 'json', ['groups' => 'getUsers']);
+        $jsonUser = $serializer->serialize($oneUser, 'json', ['groups' => 'getUsers']);
         
-        return new JsonResponse($jsonUserList, Response::HTTP_OK, [], true);
+        return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
     }
 
     #[Route('api/client/{idClient}/users/{id}', name: 'deleteUser', methods: ['DELETE'])]
@@ -43,8 +44,22 @@ class UserController extends AbstractController
     }
 
     #[Route('api/client/{idClient}/users', name: 'createUser', methods: ['POST'])]
-    public function createOneUser($idClient, Request $request, EntityManagerInterface $em, SerializerInterface $serializer): JsonResponse
+    public function createOneUser($idClient, Request $request, ClientRepository $clientRepository, EntityManagerInterface $em, SerializerInterface $serializer): JsonResponse
     {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $content = $request->toArray();
+        $idClient = $content['idClient'];
+        $user->setClient($clientRepository->find($idclient));
+        
+        $em->persist($user);
+        $em->flush();
+
+        $oneUser = $userRepository->findUser($idClient, $id);
+        $jsonUser = $serializer->serialize($oneUser, 'json', ['groups' => 'getUsers']);
+        $location = $urlGenerator->generate('detailUser', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["Location" => $location], true);
         
     }
 }
