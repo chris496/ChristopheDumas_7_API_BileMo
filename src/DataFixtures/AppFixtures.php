4bf6de65a4329b6c04f9aa288a248b9c9816f808
Christophe Dumas <cdumas@servitex.fr>
2022-09-25 21:48:43 +0200

    📦 NEW: update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,10 +4,12 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\User;
+use App\Entity\Client;
 use DateTimeImmutable;
 use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 
@@ -17,11 +19,50 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+    
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
         $products = ['Iphone 13', 'Samsung S22', 'Oppo find x5', 'Google pixel 6', 'Xiaomi Mi 11', 'Honor Magic 4', 'Sony Xperia 1'];
+
+        $client = new Client();
+        $client->setEmail("user@bilemo.com");
+        $client->setRoles(["ROLE_USER"]);
+        $client->setName($faker->Name());
+        $client->setCreatedAt(new DateTimeImmutable());
+        $client->setPassword($this->userPasswordHasher->hashPassword($client, "password"));
+        $manager->persist($client);
+
+        // Création d'un user admin
+        $client = new Client();
+        $client->setEmail("admin@bilemo.com");
+        $client->setRoles(["ROLE_ADMIN"]);
+        $client->setName($faker->Name());
+        $client->setCreatedAt(new DateTimeImmutable());
+        $client->setPassword($this->userPasswordHasher->hashPassword($client, "password"));
+        $manager->persist($client);
+
+        // create client
+        for ($i = 0; $i < 5; $i++) {
+            $client = new Client;
+            $client->setCreatedAt(new DateTimeImmutable())
+                ->setEmail($faker->safeEmail)
+                ->setName($faker->Name());
+
+            //$password = $this->encoder->hashPassword($user, 'password');
+            
+            $client->setPassword('password');
+            $manager->persist($client);
+
+            $listClient[] = $client;
+        }
 
         // create user
         for ($i = 0; $i < 20; $i++) {
@@ -30,7 +71,8 @@ class AppFixtures extends Fixture
                 ->setEmail($faker->safeEmail)
                 ->setFirstname($faker->firstName())
                 ->setLastname($faker->lastName())
-                ->setPassword('password');
+                ->setPassword('password')
+                ->setClient($listClient[array_rand($listClient)]);
 
             $manager->persist($user);
         }
