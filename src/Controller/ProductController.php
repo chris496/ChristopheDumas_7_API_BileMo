0e3ecec9d9b4145a617bfabe62a653ed67002c90
Christophe Dumas <cdumas@servitex.fr>
2022-09-26 00:24:42 +0200

    📦 NEW: cache for product and user

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,12 +18,16 @@ class ProductController extends AbstractController
 {
     #[Route('api/products', name: 'product', methods: ['GET'])]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants')]
-    public function getProductList(Request $request, ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    public function getProductList(Request $request, ProductRepository $productRepository, SerializerInterface $serializer, TagAwareCacheInterface $cachePool): JsonResponse
     {
         $page = $request->get('page', 1);
         $limit = $request->get('limit', 3);
 
-        $productList = $productRepository->findAllWithPagination($page, $limit);
+        $idCache = "getAllProducts-" . $page . "-" . $limit;
+        $productList = $cachePool->get($idCache, function (ItemInterface $item) use ($productRepository, $page, $limit) {
+            $item->tag("productsCache");
+            return $productRepository->findAllWithPagination($page, $limit);
+        });
         $jsonProductList = $serializer->serialize($productList, 'json');
         
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
